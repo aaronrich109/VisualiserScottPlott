43a6043e64d59f6f920fc89495dcdb03ce685674
Aaron Rich <18RichA@fulford.york.sch.uk>
2023-12-01 10:09:14 +0000

    I changed the status bar

--- Visualiser/Visualiser.Designer.cs
+++ Visualiser/Visualiser.Designer.cs
@@ -54,8 +54,8 @@
             // toolStripStatusLabel1
             // 
             toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            toolStripStatusLabel1.Size = new Size(154, 17);
-            toolStripStatusLabel1.Text = "Click plot to display a graph";
+            toolStripStatusLabel1.Size = new Size(163, 17);
+            toolStripStatusLabel1.Text = "Aaron - welcome to visualiser";
             // 
             // menuStrip1
             // 
